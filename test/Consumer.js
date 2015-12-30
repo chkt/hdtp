38f27f755d9221a639e94dc6111f2545d1028c2a
chkt <ck@christoph-kettelhoit.de>
2015-12-30 16:23:18 +0000

    Moved tests from Consumer to Node

--- test/Consumer.js
+++ test/Consumer.js
@@ -1,60 +1,10 @@
 import assert from 'assert';
 
-import FilterTransform from '../source/FilterTransform';
 import Consumer from '../source/Consumer';
 
 
 
 describe('Consumer', () => {
-	describe('#constructor', () => {
-		it("should accept a function as first argument", () => {
-			assert.throws(() => new Consumer(), TypeError);
-			assert.throws(() => new Consumer(true), TypeError);
-			assert.throws(() => new Consumer(1), TypeError);
-			assert.throws(() => new Consumer("1"), TypeError);
-			assert.throws(() => new Consumer(Symbol()), TypeError);
-			assert.throws(() => new Consumer({}), TypeError);
-			assert.throws(() => new Consumer([]), TypeError);
-			assert.doesNotThrow(() => new Consumer(data => 1));
-		});
-
-		it("should return a new instance", () => {
-			const consumer = new Consumer(data => 1);
-
-			assert(consumer instanceof Consumer);
-		});
-	});
-
-
-	describe('#requestTransform', () => {
-		it("should be of type FilterTransform", () => {
-			const consumer = new Consumer(data => 1);
-
-			assert(consumer.requestTransform instanceof FilterTransform);
-		});
-
-		it("should be readonly", () => {
-			const consumer = new Consumer(data => 1);
-
-			assert.throws(() => consumer.requestTransform = true);
-		});
-	});
-
-	describe('#replyTransform', () => {
-		it("should be of type FilterTransform", () => {
-			const consumer = new Consumer(data => 1);
-
-			assert(consumer.replyTransform instanceof FilterTransform);
-		});
-
-		it("should be readonly", () => {
-			const consumer = new Consumer(data => 1);
-
-			assert.throws(() => consumer.replyTransform = true);
-		});
-	});
-
-
 	describe('#target', () => {
 		it("should be of type function", () => {
 			const consumer = new Consumer(data => 1);
@@ -84,64 +34,6 @@ describe('Consumer', () => {
 			assert.strictEqual(consumer.target, fn);
 		});
 	});
-
-
-	describe('#consume', () => {
-		it("should accept any object as first argument", () => {
-			const consumer = new Consumer(data => Promise.resolve(data));
-
-			assert.throws(() => consumer.consume(true), TypeError);
-			assert.throws(() => consumer.consume(1), TypeError);
-			assert.throws(() => consumer.consume("1"), TypeError);
-			assert.throws(() => consumer.consume(Symbol()), TypeError);
-			assert.throws(() => consumer.consume(null));
-			assert.doesNotThrow(() => consumer.consume({}));
-			assert.doesNotThrow(() => consumer.consume([]));
-			assert.doesNotThrow(() => consumer.consume(() => 1));
-			assert.throws(() => consumer.consume(undefined));
-			assert.throws(() => consumer.consume(NaN));
-		});
-
-		it("should return a promise", () => {
-			const consumer = new Consumer(data => Promise.resolve(data));
-			const ret = consumer.consume({});
-
-			assert(ret instanceof Promise);
-		});
-
-		it("should require the target callback to return a promise", () => {
-			assert.throws(() => new Consumer(data => 1).consume({}), ReferenceError);
-			assert.throws(() => new Consumer(data => ({})).consume({}), ReferenceError);
-			assert.doesNotThrow(() => new Consumer(data => Promise.resolve(data)));
-		});
-
-		it("should return a promise resolving to null if aborted inside request filter", done => {
-			const consumer = new Consumer(data => Promise.resolve(data));
-
-			consumer.requestTransform.append(data => null);
-			consumer.consume({})
-				.then(val => {
-					if (val === null) done();
-					else done(new Error());
-				}, why => {
-					done(new Error());
-				});
-		});
-
-		it("should return a promise resolving to null if aborted inside reply filter", done => {
-			const consumer = new Consumer(data => Promise.resolve(data));
-
-			consumer.replyTransform.append(data => null);
-			consumer.consume({})
-				.then(val => {
-					if (val === null) done();
-					else done(new Error());
-				}, why => {
-					done(new Error());
-				});
-		});
-	});
-
 
 	describe('.Configure', () => {
 		it("should accept a function as first argument", () => {
--- test/Node.js
+++ test/Node.js
@@ -1,5 +1,6 @@
 import assert from 'assert';
 
+import FilterTransform from '../source/FilterTransform';
 import {
 	Node,
 	getTarget,
@@ -23,25 +24,107 @@ describe('setTarget', () => {
 
 describe('Node', () => {
 	describe('#constructor', () => {
-		it("should accept a function as first argument");
-		it("should return a new instance");
+		it("should accept a function as first argument", () => {
+			assert.throws(() => new Node(), TypeError);
+			assert.throws(() => new Node(true), TypeError);
+			assert.throws(() => new Node(1), TypeError);
+			assert.throws(() => new Node("1"), TypeError);
+			assert.throws(() => new Node(Symbol()), TypeError);
+			assert.throws(() => new Node({}), TypeError);
+			assert.throws(() => new Node([]), TypeError);
+			assert.doesNotThrow(() => new Node(data => 1));
+		});
+
+		it("should return a new instance", () => {
+			const node = new Node(data => 1);
+
+			assert(node instanceof Node);
+		});
 	});
 
-	describe("#requestTransform", () => {
-		it("should be of type FilterTransform");
-		it("should be readonly");
+
+	describe('#requestTransform', () => {
+		it("should be of type FilterTransform", () => {
+			const node = new Node(data => 1);
+
+			assert(node.requestTransform instanceof FilterTransform);
+		});
+
+		it("should be readonly", () => {
+			const node = new Node(data => 1);
+
+			assert.throws(() => node.requestTransform = true);
+		});
 	});
 
-	describe("#replyTransform", () => {
-		it("should be of type FilterTransform");
-		it("should be readonly");
+	describe('#replyTransform', () => {
+		it("should be of type FilterTransform", () => {
+			const node = new Node(data => 1);
+
+			assert(node.replyTransform instanceof FilterTransform);
+		});
+
+		it("should be readonly", () => {
+			const node = new Node(data => 1);
+
+			assert.throws(() => node.replyTransform = true);
+		});
 	});
 
-	describe("#consume", () => {
-		it("should accept any object as first argument");
-		it("should return a promise");
-		it("should require the target callback to return a promise");
-		it("should return a promise resolving to null if aborted inside request filter");
-		it("should return a promise resolving to null if aborted inside reply filter");
+
+	describe('#consume', () => {
+		it("should accept any object as first argument", () => {
+			const node = new Node(data => Promise.resolve(data));
+
+			assert.throws(() => node.consume(true), TypeError);
+			assert.throws(() => node.consume(1), TypeError);
+			assert.throws(() => node.consume("1"), TypeError);
+			assert.throws(() => node.consume(Symbol()), TypeError);
+			assert.throws(() => node.consume(null));
+			assert.doesNotThrow(() => node.consume({}));
+			assert.doesNotThrow(() => node.consume([]));
+			assert.doesNotThrow(() => node.consume(() => 1));
+			assert.throws(() => node.consume(undefined));
+			assert.throws(() => node.consume(NaN));
+		});
+
+		it("should return a promise", () => {
+			const node = new Node(data => Promise.resolve(data));
+			const ret = node.consume({});
+
+			assert(ret instanceof Promise);
+		});
+
+		it("should require the target callback to return a promise", () => {
+			assert.throws(() => new Node(data => 1).consume({}), ReferenceError);
+			assert.throws(() => new Node(data => ({})).consume({}), ReferenceError);
+			assert.doesNotThrow(() => new Node(data => Promise.resolve(data)));
+		});
+
+		it("should return a promise resolving to null if aborted inside request filter", done => {
+			const node = new Node(data => Promise.resolve(data));
+
+			node.requestTransform.append(data => null);
+			node.consume({})
+				.then(val => {
+					if (val === null) done();
+					else done(new Error());
+				}, why => {
+					done(new Error());
+				});
+		});
+
+		it("should return a promise resolving to null if aborted inside reply filter", done => {
+			const node = new Node(data => Promise.resolve(data));
+
+			node.replyTransform.append(data => null);
+			node.consume({})
+				.then(val => {
+					if (val === null) done();
+					else done(new Error());
+				}, why => {
+					done(new Error());
+				});
+		});
 	});
 });
