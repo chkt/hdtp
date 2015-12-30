07970d8c6e46896a353ba22eb50e55e18db8e7bc
chkt <ck@christoph-kettelhoit.de>
2015-12-30 00:43:30 +0000

    Changed Consumer to inherit from Node

--- source/Consumer.js
+++ source/Consumer.js
@@ -1,15 +1,12 @@
-import Filter from './FilterTransform';
+import {
+	Node,
+	getTarget,
+	setTarget
+} from './Node';
 
 
 
-const _target = new WeakMap();
-
-const _requestTransform = new WeakMap();
-const _replyTransform = new WeakMap();
-
-
-
-export default class Consumer {
+export default class Consumer extends Node {
 
 	/**
 	 * Returns a configured instance
@@ -36,43 +33,11 @@ export default class Consumer {
 
 
 	/**
-	 * Creates a new instance
-	 * @param {Function} fn - The callback
-	 * @throws {TypeError} if fn is not a Function
-	 */
-	constructor(fn) {
-		if (typeof fn !== 'function') throw new TypeError();
-
-		_target.set(this, fn);
-
-		_requestTransform.set(this, new Filter());
-		_replyTransform.set(this, new Filter());
-	}
-
-
-	/**
-	 * Gets the request filter
-	 * @returns {Filter}
-	 */
-	get requestTransform() {
-		return _requestTransform.get(this);
-	}
-
-	/**
-	 * Gets the reply filter
-	 * @returns {Filter}
-	 */
-	get replyTransform() {
-		return _replyTransform.get(this);
-	}
-
-
-	/**
 	 * Gets the target callback
 	 * @returns {Function}
 	 */
 	get target() {
-		return _target.get(this);
+		return getTarget.call(this);
 	}
 
 	/**
@@ -81,30 +46,6 @@ export default class Consumer {
 	 * @throws {TypeError} if fn is not a Function
 	 */
 	set target(fn) {
-		if (typeof fn !== 'function') throw new TypeError();
-
-		_target.set(this, fn);
-	}
-
-
-	/**
-	 * Returns a promise representing the transformed result
-	 * @param {Object} data - The request data
-	 * @returns {Promise}
-	 * @throws {TypeError} if data is not an Object
-	 * @throws {ReferenceError} if the callback return value is not a Promise
-	 */
-	consume(data) {
-		if (!(data instanceof Object)) throw new TypeError();
-
-		data = _requestTransform.get(this).process(data);
-
-		if (data === null) return Promise.resolve(null);
-
-		let p = _target.get(this)(data);
-
-		if (!(p instanceof Promise)) throw new ReferenceError();
-
-		return p.then(data => _replyTransform.get(this).process(data));
+		setTarget.call(this, fn);
 	}
 }
